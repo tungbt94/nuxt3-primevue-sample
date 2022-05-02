c2d5bf353e2a6160ce822c5bc9564b2753ec3208
tungbt94 <tungbt94@gmail.com>
2022-05-02 21:46:11 +0700

    feat: add prime vue

---
+++ components/Tabs.vue
@@ -0,0 +1,155 @@
+<template>
+  <div>
+    <div class="card">
+        <h5>Default</h5>
+        <tab-view ref="tabview1">
+            <tab-panel header="Header I">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </tab-panel>
+            <tab-panel header="Header II">
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.</p>
+            </tab-panel>
+            <tab-panel header="Header III">
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.</p>
+            </tab-panel>
+        </tab-view>
+    </div>
+
+    <div class="card">
+        <h5>Programmatic</h5>
+        <div class="py-2">
+            <Button @click="active1 = 0" class="p-button-text" label="Activate 1st" />
+            <Button @click="active1 = 1" class="p-button-text mr-2" label="Activate 2nd" />
+            <Button @click="active1 = 2" class="p-button-text mr-2" label="Activate 3rd" />
+        </div>
+
+        <tab-view ref="tabview2" v-model:activeIndex="active1">
+            <tab-panel header="Header I">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </tab-panel>
+            <tab-panel header="Header II">
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.</p>
+            </tab-panel>
+            <tab-panel header="Header III">
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.</p>
+            </tab-panel>
+        </tab-view>
+    </div>
+
+    <div class="card">
+        <h5>Disabled</h5>
+        <tab-view ref="tabview3">
+            <tab-panel header="Header I">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </tab-panel>
+            <tab-panel header="Header II">
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.</p>
+            </tab-panel>
+            <tab-panel header="Header III">
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.</p>
+            </tab-panel>
+            <tab-panel header="Header IV" :disabled="true"></tab-panel>
+        </tab-view>
+    </div>
+
+    <div class="card">
+        <h5>Custom Headers</h5>
+        <tab-view class="tabview-custom" ref="tabview4">
+            <tab-panel>
+                <template #header>
+                    <i class="pi pi-calendar"></i>
+                    <span>Header I</span>
+                </template>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </tab-panel>
+            <tab-panel>
+                <template #header>
+                    <span>Header II</span>
+                    <i class="pi pi-user"></i>
+                </template>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+                    architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                    voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.</p>
+            </tab-panel>
+            <tab-panel>
+                <template #header>
+                    <i class="pi pi-search"></i>
+                    <span>Header III</span>
+                    <i class="pi pi-cog"></i>
+                </template>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati
+                    cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
+                    Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.</p>
+            </tab-panel>
+        </tab-view>
+    </div>
+
+    <div class="card">
+        <h5>Dynamic Tabs</h5>
+        <tab-view>
+            <tab-panel v-for="tab in tabs" :key="tab.title" :header="tab.title">
+                <p>{{tab.content}}</p>
+            </tab-panel>
+        </tab-view>
+    </div>
+
+    <div class="card">
+        <h5>Scrollable</h5>
+        <div class="py-2">
+            <Button @click="active2 = 0" class="p-button-text" label="Activate 1st" />
+            <Button @click="active2 = 29" class="p-button-text mr-2" label="Activate 30th" />
+            <Button @click="active2 = 49" class="p-button-text mr-2" label="Activate 50th" />
+        </div>
+
+        <tab-view v-model:activeIndex="active2" :scrollable="true">
+            <tab-panel v-for="tab in scrollableTabs" :key="tab.title" :header="tab.title">
+                <p>{{tab.content}}</p>
+            </tab-panel>
+        </tab-view>
+    </div>
+</div>
+</template>
+
+<script setup lang="ts">
+import TabView from 'primevue/tabview'
+import TabPanel from 'primevue/tabpanel'
+import Button from 'primevue/button'
+
+const active1 = ref(0)
+const active2 = ref(0)
+const tabs = ref([
+  {
+    title: 'Header I',
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    title: 'Header II',
+    content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi."
+  },
+  {
+    title: 'Header III',
+    content: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus."
+  }
+])
+const scrollableTabs = ref(Array.from({ length: 50 }, (_, i) => ({ title: `Tab ${i + 1}`, content: `Tab ${i + 1} Content` })))
+
+</script>
+
+<style scoped>
+.tabview-custom {
+    i, span {
+        vertical-align: middle;
+    }
+
+    span {
+        margin: 0 .5rem;
+    }
+}
+
+.p-tabview p {
+    line-height: 1.5;
+    margin: 0;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container m-1">
+    <div class="header">
+      Header
+    </div>
+
+    <div class="main">
+      Main
+      <slot></slot>
+    </div>
+
+    <div class="footer">
+      Footer
+    </div>
+  </div>
+</template>
